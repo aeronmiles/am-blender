c4397dd2bffa433bb53cc940f4342c045c3fd4c7
Aeron <aeronmiles@yahoo.co.uk>
2022-12-23 21:21:05 +0000

    Add mac os, linux install.sh support

--- install.sh
+++ install.sh
@@ -1,71 +1,52 @@
 #!/usr/bin/env bash
 
 read -p "Enter Blender version e.g. 3.3 > " VER
-DIR="${APPDATA}/Blender Foundation/Blender/${VER}"
-if [ ! -d "$DIR" ]; then
-    read -p "Blender version ${VER} doesn't exist, please enter correct version > " VER
-    DIR="${APPDATA}/Blender Foundation/Blender/${VER}"
-fi
-if [ ! -d "$DIR" ]; then
+
+# Determine the user's addons directory based on the operating system
+case "$(uname -sr)" in
+Darwin*)
+    # macOS
+    USER_PATH="/Users/${USER}/Library/Application Support/Blender/${VER}"
+    ;;
+Linux*Microsoft*)
+    # WSL
+    USER_PATH="${APPDATA}/Blender Foundation/Blender/${VER}"
+    ;;
+Linux*)
+    # Linux
+    USER_PATH="${HOME}/.config/blender/${VER}"
+    ;;
+CYGWIN* | MINGW* | MINGW32* | MSYS*)
+    # MS Windows
+    USER_PATH="${APPDATA}/Blender Foundation/Blender/${VER}"
+    ;;
+*)
+    # Unsupported operating system
+    echo "Install not supported on this OS"
+    exit 1
+    ;;
+esac
+
+if [ ! -d "$USER_PATH" ]; then
     echo "Blender version ${VER} doesn't exist, exiting."
     exit 1
 fi
 
-case "$(uname -sr)" in
+# Install the addons
+# USDZ
+if [ -d "$USER_PATH/scripts/addons/io_scene_usdz" ]; then
+    rm -r "$USER_PATH/scripts/addons/io_scene_usdz"
+fi
+cp -r ./extern/addons/BlenderUSDZ/io_scene_usdz "$USER_PATH/scripts/addons"
 
-Darwin*)
-    echo 'Mac OS install not implemented, feel free to submit a merge request. Otherwise, keep calm and carry on.'
-    ;;
+# reload_addons
+if [ -f "$USER_PATH/scripts/addons/reload_addons.py" ]; then
+    rm -f "$USER_PATH/scripts/addons/reload_addons.py"
+fi
+cp ./extern/addons/john-kanji/reload_addons.py "$USER_PATH/scripts/addons"
 
-Linux*Microsoft*)
-    echo 'WSL install not implemented, feel free to submit a merge request. Otherwise, keep calm and carry on.'
-    ;;
-
-Linux*)
-    echo 'Linux install not implemented, feel free to submit a merge request. Otherwise, keep calm and carry on.'
-    ;;
-
-CYGWIN* | MINGW* | MINGW32* | MSYS*)
-    # MS Windows
-
-    # read -p "Install configs (y/n)? " CPCONF
-    # case "$CPCONF" in
-    # y | Y)
-    #     cp -r ./config "$DIR"
-    #     ;;
-    # *) ;;
-    # esac
-
-    # read -p "Install scripts (y/n)? " CPSCRIPTS
-    # case "$CPSCRIPTS" in
-    # y | Y)
-    #     cp -r ./HEAVYPOLY_Blender/scripts "$DIR"
-    #     cp -r ./scripts "$DIR"
-    #     ;;
-    # *) ;;
-    # esac
-
-    ### USDZ
-    if [ -d "$DIR/scripts/addons/io_scene_usdz" ]; then
-        rm -r "$DIR/scripts/addons/io_scene_usdz"
-    fi
-    cp -r ./extern/addons/BlenderUSDZ/io_scene_usdz "$DIR/scripts/addons"
-
-    ### reload_addons
-    if [ -f "$DIR/scripts/addons/reload_addons.py" ]; then
-        rm -f "$DIR/scripts/addons/reload_addons.py"
-    fi
-    cp ./extern/addons/john-kanji/reload_addons.py "$DIR/scripts/addons"
-
-    ### am-blender
-    if [ -d "$DIR/scripts/addons/am-blender" ]; then
-        rm -r "$DIR/scripts/addons/am-blender"
-    fi
-    cp -r ./scripts "$DIR"
-
-    ;;
-*)
-
-    echo 'Install not supported on this OS'
-    ;;
-esac
+### am-blender
+if [ -d "$USER_PATH/scripts/addons/am-blender" ]; then
+    rm -r "$USER_PATH/scripts/addons/am-blender"
+fi
+cp -r ./scripts "$USER_PATH"
